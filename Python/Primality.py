4db8aa78ff2117cd458bee1dc98251bdb747ea58
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2020-03-10 16:32:28 +0100

    fixes

--- Python/Primality.py
+++ Python/Primality.py
@@ -1,20 +1,20 @@
 #Check if n is a composite, if yes, then return a fermat_witness
 def fermat_witness(n):
-  for a in range(2,n-2):
-    if pow(a, n-1, n) != 1:
-      return a
+	for a in range(2,n-2):
+		if pow(a, n-1, n) != 1:
+			return a
 
 #Miller-Rabin primality test
 def test_miller_rabin(n,k):
-		for i in range(k):
-			a = random.randint(2,n-2)
+	for i in range(k):
+		a = random.randint(2,n-2)
 			if pow(a, n-1, n)!=1:
 				return False
 		return True
     
 #Find a prime number in a given range (a --> b)
 def prime_in_range(a,b):
-  for i in range(a, b):
-	  if test_miller_rabin(i,32):
-		  return i	
+	for i in range(a, b):
+		if test_miller_rabin(i,32):
+			return i	
    
