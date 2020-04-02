04bc85923094557d0d5277e75a5af7a905b2055a
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2020-04-02 17:16:15 +0200

    add linear congruence and extdgcd algorithms

--- Python/Arithmetic.py
+++ Python/Arithmetic.py
@@ -1,10 +1,16 @@
+#Compute extended gcd of two integers (Extended Euclidean algorithm)
 def extd_pgcd(a, b):
-		r, r2 = a, b 
-		u = v2 = 1
-		u2 = v = 0
-		while r2 != 0:
-			q = r//r2
-			rs, us, vs = r, u, v
-			r, u, v = r2, u2, v2
-			r2, u2, v2 = (rs - q*r2), (us - q*u2), (vs - q*v2)
-		return u
+	r, r2 = a, b 
+	u = v2 = 1
+	u2 = v = 0
+	while r2 != 0:
+		q = r//r2
+		rs, us, vs = r, u, v
+		r, u, v = r2, u2, v2
+		r2, u2, v2 = (rs - q*r2), (us - q*u2), (vs - q*v2)
+	return u
+
+#Resolve linear congruence equations
+def linear_congruence(a, b, n):
+	c = extd_pgcd(a, n)
+	return ((c * (-b%n)) % n)
