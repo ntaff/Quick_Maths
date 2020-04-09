20d7d768af90c30ed69d555358f0b8dadfdc68fd
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2020-04-09 11:21:21 +0200

    Create Primality.exs

---
+++ Elixir/Primality.exs
@@ -0,0 +1,35 @@
+defmodule PrimeFinder do
+  def nth_prime(n) do
+    prime_sequence()
+    |> Stream.take(n)
+    |> Enum.to_list
+    |> List.last
+  end
+
+  def prime_sequence do
+    Stream.unfold(2, fn(x) ->
+      if(x |> prime?) do
+        {x, x+1}
+      else
+        {nil, x+1}
+      end
+    end)
+    |> Stream.reject(&is_nil/1)
+  end
+
+  def prime?(x) when x == 2, do: true
+  def prime?(x) when x in [1, 4], do: false
+  def prime?(x) do
+    2..round(:math.sqrt(x))
+    |> Enum.reduce_while(true, fn(e, _) ->
+      if(0 == rem(x, e)) do
+        {:halt, false}
+      else
+        {:cont, true}
+      end
+    end)
+  end
+end
+
+# Return the 999th prime number
+# PrimeFinder.nth_prime(999) |> IO.puts
