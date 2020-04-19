fb93eafd8b8952fecad430bb70b05cd97de2aade
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2020-04-19 12:00:47 +0200

    fix

--- Elixir/Numbers.exs
+++ Elixir/Numbers.exs
@@ -1,11 +1,11 @@
 # Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital
 def pandigital_products() do
     for x <- 1..9999,
-		    y <- 1..99,
-		    xy = x * y,
-		    '123456789' == Enum.sort(to_charlist(Integer.to_string(x) <> Integer.to_string(y) <> Integer.to_string(xy)))
-	  do xy end
+	    y <- 1..99,
+	    xy = x * y,
+	    '123456789' == Enum.sort(to_charlist(Integer.to_string(x) <> Integer.to_string(y) <> Integer.to_string(xy)))
+    do xy end
     |> Enum.uniq
-	  |> Enum.sum
+    |> Enum.sum
     |> IO.puts
 end
